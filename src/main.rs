de3870ec195a4b8213318e259ddb6881705e6a76
Fred Hsu <fredlhsu@gmail.com>
2022-12-01 21:54:22 -0800

    day1

---
+++ src/main.rs
@@ -0,0 +1,26 @@
+use std::env;
+use std::fs;
+
+fn main() {
+    let file_path = "day1-input.txt";
+
+    let contents = fs::read_to_string(file_path).expect("Should have been able to read the file");
+
+    let mut calorie_count = 0;
+    let mut elves = Vec::new();
+    for line in contents.lines() {
+        if line.is_empty() {
+            elves.push(calorie_count);
+            // set up next elf
+            calorie_count = 0;
+        } else {
+            calorie_count += line.parse::<u32>().unwrap();
+        }
+    }
+    elves.sort();
+    let biggest = elves.pop().unwrap();
+    println!("Most calories {}", biggest);
+    let top3 = biggest + elves.pop().unwrap() + elves.pop().unwrap();
+
+    println!("Top 3 calories {}", top3);
+}
