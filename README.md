9a7512d19bd78797619105c77e448ee9fb067e56
Fred Hsu <fredlhsu@gmail.com>
2022-12-01 22:20:54 -0800

    cleanup

---
+++ README.md
@@ -0,0 +1,5 @@
+# Advent of code 2022
+My solutions to the AoC for 2022 in Rust:
+https://adventofcode.com/2022
+
+
--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,7 @@
-use std::env;
 use std::fs;
 
-fn main() {
+fn day_one() -> Result<(), std::num::ParseIntError>{
     let file_path = "day1-input.txt";
-
     let contents = fs::read_to_string(file_path).expect("Should have been able to read the file");
 
     let mut calorie_count = 0;
@@ -14,13 +12,18 @@ fn main() {
             // set up next elf
             calorie_count = 0;
         } else {
-            calorie_count += line.parse::<u32>().unwrap();
+            calorie_count += line.parse::<u32>()?;
         }
     }
     elves.sort();
     let biggest = elves.pop().unwrap();
     println!("Most calories {}", biggest);
-    let top3 = biggest + elves.pop().unwrap() + elves.pop().unwrap();
 
+    let top3 = biggest + elves.pop().unwrap() + elves.pop().unwrap();
     println!("Top 3 calories {}", top3);
+    Ok(())
+}
+
+fn main() {
+    day_one();
 }
