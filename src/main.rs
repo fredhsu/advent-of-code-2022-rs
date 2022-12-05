71ab4ddfd156d040f7d5e7dedcad5aa3c5728a8c
Fred Hsu <fredlhsu@gmail.com>
2022-12-04 19:44:29 -0800

    day4

--- src/main.rs
+++ src/main.rs
@@ -151,6 +151,45 @@ fn day_three() {
     day3_part2_chunk(&contents);
 }
 
+fn day_four() {
+    let file_path = "day4-input.txt";
+    let contents = fs::read_to_string(file_path).expect("Should have been able to read the file");
+    let mut total = 0;
+    let mut overlap = 0;
+    for line in contents.lines() {
+        // switch to split_once and get a tuple
+        let pair = line.split(',');
+        let pair2 = pair
+            .map(|x| {
+                x.split("-")
+                    .map(|x| x.parse::<u32>().unwrap())
+                    .collect::<Vec<u32>>()
+            })
+            .collect::<Vec<Vec<u32>>>();
+        if pair2[0][1] >= pair2[1][0] && pair2[0][0] <= pair2[1][1] {
+            println!("Overlap: {:?}", pair2);
+            println!("overlap: {} >= {} ", pair2[0][1], pair2[1][0]);
+            overlap += 1;
+        } else {
+            println!("Does not overlap {:?}", pair2);
+        }
+        if pair2[0][0] <= pair2[1][0] && pair2[0][1] >= pair2[1][1] {
+            println!(
+                "first pair fits: {} <= {} and {} >= {}",
+                pair2[0][0], pair2[0][1], pair2[0][1], pair2[1][1]
+            );
+            total += 1;
+            continue;
+        } else if pair2[1][0] <= pair2[0][0] && pair2[1][1] >= pair2[0][1] {
+            println!("second pair fits: {:?}", pair2);
+            total += 1;
+        } else {
+            println!("does not fit");
+        }
+    }
+    println!("total: {} ", total);
+    println!("overlap: {}", overlap);
+}
 fn main() {
-    day_three();
+    day_four();
 }
