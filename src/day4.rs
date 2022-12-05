90be89577d7d858b4a865f6db023a9237dd0c4e0
Fred Hsu <fredlhsu@gmail.com>
2022-12-04 20:00:58 -0800

    organizing code

--- src/day4.rs
+++ src/day4.rs
@@ -5,9 +5,13 @@ pub fn day_four() {
     let mut total = 0;
     let mut overlap = 0;
     for line in contents.lines() {
-        // switch to split_once and get a tuple
-        let pair = line.split(',');
-        let pair2 = pair
+        // Attempted to switch to split_once and get a tuple, but cannot map over tuple making things
+        // more clunky to apply splits and parsing to each member
+        //let tup = line.split_once(',').unwrap();
+        //let tups = (tup.0.split_once("-"), tup.1.split_once("-"));
+        //let tups = tup.map(|x| x.split_once("-").map(|x| x.parse::<u32>().unwrap()).collect::<Vec<u32>>())
+        let pair2 = line
+            .split(',')
             .map(|x| {
                 x.split("-")
                     .map(|x| x.parse::<u32>().unwrap())
@@ -19,20 +23,15 @@ pub fn day_four() {
         // first is below the upper bound of the second otherwise it would
         // be completely outside for ex:  3-7, 8-9
         if pair2[0][1] >= pair2[1][0] && pair2[0][0] <= pair2[1][1] {
-            println!("Overlap: {:?}", pair2);
             overlap += 1;
         } else {
             println!("Does not overlap {:?}", pair2);
         }
         if pair2[0][0] <= pair2[1][0] && pair2[0][1] >= pair2[1][1] {
-            println!(
-                "first pair fits: {} <= {} and {} >= {}",
-                pair2[0][0], pair2[0][1], pair2[0][1], pair2[1][1]
-            );
+            println!(pair2[0][0], pair2[0][1], pair2[0][1], pair2[1][1]);
             total += 1;
             continue;
         } else if pair2[1][0] <= pair2[0][0] && pair2[1][1] >= pair2[0][1] {
-            println!("second pair fits: {:?}", pair2);
             total += 1;
         } else {
             println!("does not fit");
