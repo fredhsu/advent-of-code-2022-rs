1ddc8a7f36230f56e6487f5630bfa1c61e2a5d5e
Fred Hsu <fredlhsu@gmail.com>
2022-12-13 10:39:29 -0800

    day9 part1

--- src/day9.rs
+++ src/day9.rs
@@ -1,3 +1,5 @@
+use std::collections::HashSet;
+
 type Point = (i32, i32);
 fn is_adjacent(head: &Point, tail: &Point) -> bool {
     !((head.0 - tail.0).abs() >= 2 || (head.1 - tail.1).abs() >= 2)
@@ -8,28 +10,85 @@ fn is_adjacent(head: &Point, tail: &Point) -> bool {
     true
     */
 }
-fn move_right(dist: u32, head: &mut Point, tail: &mut Point) {
-    for i in 0..dist {
+fn move_right(dist: u32, head: &mut Point, tail: &mut Point) -> Vec<Point>{
+    let mut moves = Vec::new();
+    for _ in 0..dist {
         head.1 += 1;
-        println!("adjacent: {}", is_adjacent(head, tail));
+        if !is_adjacent(head, tail) {
+            tail.1 += 1;
+            tail.0 = tail.0 + (head.0 - tail.0);
+            moves.push((tail.0, tail.1));
+        }
     }
+    moves
 }
+fn move_left(dist: u32, head: &mut Point, tail: &mut Point) -> Vec<Point>{
+    let mut moves = Vec::new();
+    for _ in 0..dist {
+        head.1 -= 1;
+        if !is_adjacent(head, tail) {
+            tail.1 -= 1;
+            tail.0 = tail.0 + (head.0 - tail.0);
+            moves.push((tail.0, tail.1));
+        }
+    }
+    moves
+}
+fn move_up(dist: u32, head: &mut Point, tail: &mut Point) -> Vec<Point>{
+    let mut moves = Vec::new();
+    for _ in 0..dist {
+        head.0 += 1;
+        if !is_adjacent(head, tail) {
+            tail.0 += 1;
+            tail.1 = tail.1 + (head.1 - tail.1);
+            moves.push((tail.0, tail.1));
+        }
+    }
+    moves
+}
+fn move_down(dist: u32, head: &mut Point, tail: &mut Point) -> Vec<Point> {
+    let mut moves = Vec::new();
+    for _ in 0..dist {
+        head.0 -= 1;
+        if !is_adjacent(head, tail) {
+            tail.0 -= 1;
+            tail.1 = tail.1 + (head.1 - tail.1);
+            moves.push((tail.0, tail.1));
+        }
+    }
+    moves
+}
+
 pub fn day_nine() {
-    let lines = include_str!("../input/day9-test.txt").lines();
+    let lines = include_str!("../input/day9-input.txt").lines();
     let mut head = (0, 0);
     let mut tail = (0, 0);
+    let mut visited:HashSet<Point> = HashSet::new();
+    visited.insert(tail);
+    let mut tail_moves: Vec<Point> = Vec::new();
     for line in lines {
-        let (dir, dist) = line.split_once(" ").unwrap();
+        let (dir, dist) = line.split_once(' ').unwrap();
         let dist: u32 = dist.parse().unwrap();
         match dir {
             "R" => {
-                move_right(dist, &mut head, &mut tail);
+                tail_moves.append(&mut move_right(dist, &mut head, &mut tail));
             }
-            "L" => {}
-            "U" => {}
-            "D" => {}
+            "L" => {
+                tail_moves.append(&mut move_left(dist, &mut head, &mut tail));
+            }
+            "U" => {
+                tail_moves.append(&mut move_up(dist, &mut head, &mut tail));
+            }
+            "D" => {
+                tail_moves.append(&mut move_down(dist, &mut head, &mut tail));
+            }
             _ => println!("Unknown"),
         }
-        println!("head: {head:#?}, tail: {tail:#?}");
     }
+        println!("Tail moves: {:?}", tail_moves);
+        tail_moves.iter().for_each(|t| {
+            visited.insert(*t);
+        }
+        );
+        println!("Visited: {}", visited.len());
 }
