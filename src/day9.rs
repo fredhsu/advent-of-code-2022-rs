aeca46b5dda0f3b2c190ea89d770d067312de2cf
Fred Hsu <fredlhsu@gmail.com>
2022-12-14 08:42:46 -0800

    day9 cont

--- src/day9.rs
+++ src/day9.rs
@@ -3,25 +3,54 @@ use std::collections::HashSet;
 type Point = (i32, i32);
 fn is_adjacent(head: &Point, tail: &Point) -> bool {
     !((head.0 - tail.0).abs() >= 2 || (head.1 - tail.1).abs() >= 2)
-    /*
-    if (head.0 - tail.0).abs() >= 2 || (head.1 - tail.1).abs() >= 2 {
-        return false;
-    }
-    true
-    */
 }
 fn move_right(dist: u32, head: &mut Point, tail: &mut Point) -> Vec<Point>{
     let mut moves = Vec::new();
     for _ in 0..dist {
         head.1 += 1;
-        if !is_adjacent(head, tail) {
-            tail.1 += 1;
-            tail.0 = tail.0 + (head.0 - tail.0);
-            moves.push((tail.0, tail.1));
+        if let Some(new_tail) = follow_right(head, tail) {
+            *tail = new_tail;
+            moves.push(new_tail);
         }
     }
     moves
 }
+fn move_knots_right(dist: u32, knots: &mut Vec<Point>) -> Vec<Point> {
+    let mut moves = Vec::new();
+    for _ in 0..dist {
+        knots[0].1 += 1;
+        println!("Moving head to: {:?}", knots[0]);
+        for i in 0..knots.len() - 1{
+            if let Some(new_tail) = follow_right(&knots[i], &knots[i+1]) {
+               knots[i+1] = new_tail; 
+               println!("Moving knots[{}] to {:?}", i+1, new_tail);
+               if i == knots.len() - 1 {
+                   // push the new tail if we are at the last knot
+                   moves.push(new_tail);
+               }
+            }
+        }
+    }
+    moves
+}
+
+fn follow_right(lead: &Point, follow: &Point) -> Option<Point>{
+        if !is_adjacent(lead, follow) {
+            let col = follow.1 + 1;
+            let row = follow.0 + (lead.0 - follow.0);
+            return Some((row, col));
+        }
+        None
+}
+fn follow_left(lead: &Point, follow: &Point) -> Option<Point>{
+        if !is_adjacent(lead, follow) {
+            let col = follow.1 - 1;
+            let row = follow.0 + (lead.0 - follow.0);
+            return Some((row, col));
+        }
+        None
+}
+
 fn move_left(dist: u32, head: &mut Point, tail: &mut Point) -> Vec<Point>{
     let mut moves = Vec::new();
     for _ in 0..dist {
@@ -60,9 +89,10 @@ fn move_down(dist: u32, head: &mut Point, tail: &mut Point) -> Vec<Point> {
 }
 
 pub fn day_nine() {
-    let lines = include_str!("../input/day9-input.txt").lines();
+    let lines = include_str!("../input/day9-test.txt").lines();
     let mut head = (0, 0);
     let mut tail = (0, 0);
+    let mut knots = vec![(0,0); 9];
     let mut visited:HashSet<Point> = HashSet::new();
     visited.insert(tail);
     let mut tail_moves: Vec<Point> = Vec::new();
@@ -72,6 +102,7 @@ pub fn day_nine() {
         match dir {
             "R" => {
                 tail_moves.append(&mut move_right(dist, &mut head, &mut tail));
+                move_knots_right(dist, &mut knots);
             }
             "L" => {
                 tail_moves.append(&mut move_left(dist, &mut head, &mut tail));
