47bf135fa72ff2cffe97d260383b75aa93296010
fredhsu <fredlhsu@gmail.com>
2022-12-20 07:52:22 -0800

    started day13 trying to parse

--- src/day13.rs
+++ src/day13.rs
@@ -7,40 +7,19 @@ use nom::multi::separated_list1;
 use nom::sequence::*;
 use nom::IResult;
 
-fn parse_monkey(s: &str) -> IResult<&str, Monkey> {
-    let (i, _) = tuple((tag("Monkey "), u64, tag(":\n")))(s)?;
-    let (i, (_, _, items, _)) = tuple((
-        space1,
-        tag("Starting items: "),
-        separated_list1(tag(", "), u64),
-        tag("\n"),
-    ))(i)?;
-    let (i, (_, _, operation, _)) =
-        tuple((space1, tag("Operation: "), parse_operation, tag("\n")))(i)?;
-    let (i, (_, _, divisor, _)) = tuple((space1, tag("Test: divisible by "), u64, tag("\n")))(i)?;
-    let (i, (_, _, true_target, _)) =
-        tuple((space1, tag("If true: throw to monkey "), u64, tag("\n")))(i)?;
-    let (i, (_, _, false_target, _)) =
-        tuple((space1, tag("If false: throw to monkey "), u64, tag("\n")))(i)?;
-    let m = Monkey {
-        items,
-        inspections: 0,
-        operation,
-        divisor,
-        true_target: true_target as usize,
-        false_target: false_target as usize,
-    };
-    Ok((i, m))
-}
 enum Packet {
-    List,
-    Integer,
+    List(Vec<Packet>),
+    Integer(u32),
 }
-pub fn parse_packets(i: &str) -> IResult<&str, Vec<Monkey>> {
-    separated_list1(tag("\n"), parse_packet)(i)
+pub fn parse_packets(i: &str) -> IResult<&str, &str> {
+    let parser = delimited(tag("["), tag(","), tag("]"))(i)?;
+    Ok(parser)
 }
 
-fn parse_packet(i: &str) -> IResult<&str, &str> {}
+fn parse_packet(i: &str) -> IResult<&str, &str> {
+    // separated list or a single packet or delimited
+    let parser = separated_list1(",")(i)?;
+}
 
 pub fn day_thirteen() {
     let lines = include_str!("../input/day13-test.txt");
--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,7 @@ mod day1;
 mod day10;
 mod day11;
 mod day12;
+mod day13;
 mod day2;
 mod day3;
 mod day4;
@@ -14,5 +15,5 @@ mod day9;
 fn main() {
     //    let _ = day7::day_seven();
     //day9::day_nine();
-    day12::day_twelve();
+    day13::day_thirteen();
 }
