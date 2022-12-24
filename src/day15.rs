71a156db5bf8b09136e6e989017f0ea26e0108c4
fredhsu <fredlhsu@gmail.com>
2022-12-23 17:23:30 -0800

    day 15 part1

--- src/day15.rs
+++ src/day15.rs
@@ -18,24 +18,48 @@ struct Point {
 }
 */
 
-fn get_coverage(sensor: Point, beacon: Point) -> Vec<Point> {
-    println!("Coverage for sensor: {sensor:?}, beacon: {beacon:?}");
+fn get_coverage(sensor: Point, beacon: Point, row: i32) -> HashSet<Point> {
+    //println!("Coverage for sensor: {sensor:?}, beacon: {beacon:?}");
     let mut points = HashSet::new();
     let mdist = (sensor.0.abs_diff(beacon.0) + sensor.1.abs_diff(beacon.1)) as i32;
     //println!("Manhattan Distance: {mdist}");
-    for i in sensor.0 - mdist..=sensor.0 + mdist {
-        let y_dist = mdist - (i.abs_diff(sensor.0) as i32);
-        //println!("{i} : dist {y_dist}");
-        for j in 0..=y_dist {
-            let x = i;
-            let y = sensor.1 + j;
-            let y2 = sensor.1 - j;
-            points.insert((x, y));
-            points.insert((x, y2));
+    if sensor.1 + mdist > row && sensor.1 - mdist < row {
+        // figure out what x positions need to be added based on the y distance from the sensor
+        let y_dist = row.abs_diff(sensor.1) as i32;
+        let x_dist = mdist - y_dist;
+        for i in sensor.0 - x_dist..=sensor.0 + x_dist {
+            points.insert((i, row));
         }
+    } else {
+        println!(
+            "Max: {}\nRow: {row}\nMin: {}\n\n",
+            sensor.1 + mdist,
+            sensor.1 - mdist
+        );
     }
+    /*
+        for i in sensor.0 - mdist..=sensor.0 + mdist {
+            let y_dist = mdist - (i.abs_diff(sensor.0) as i32);
+            //println!("{i} : dist {y_dist}");
+            for j in 0..=y_dist {
+                points.insert(i, y);
+            }
+            /*for j in 0..=y_dist {
+                let x = i;
+                let y = sensor.1 + j;
+                let y2 = sensor.1 - j;
+                if y == row {
+                    points.insert((x, y));
+                } else if y2 == row {
+                    points.insert((x, y2));
+                }
+                //println!("Inserting for ({x}, {y}) and ({x}, {y2})");
+            }
+            */
+        }
+    */
     //println!("Points: {points:?}");
-    points.into_iter().collect()
+    points
 }
 fn parse_line(i: &str) -> IResult<&str, (Point, Point)> {
     let (rest, (_, sx, _, sy, _, bx, _, by)) = tuple((
@@ -46,12 +70,12 @@ fn parse_line(i: &str) -> IResult<&str, (Point, Point)> {
         tag(": closest beacon is at x="),
         map_res(take_until(","), str::parse),
         tag(", y="),
-        map_res(digit1, str::parse),
+        map_res(take_until("\n"), str::parse),
     ))(i)?;
     Ok((rest, ((sx, sy), (bx, by))))
 }
 
-fn print_coverage(map: Vec<Point>) {
+fn print_coverage(map: &HashSet<Point>) {
     let top = map.iter().min_by_key(|&x| x.1).unwrap().1;
     let bottom = map.iter().max_by_key(|&x| x.1).unwrap().1;
     let left = map.iter().min_by_key(|&x| x.0).unwrap().0;
@@ -69,18 +93,27 @@ fn print_coverage(map: Vec<Point>) {
     }
 }
 pub fn day_fifteen() {
-    let lines = include_str!("../input/day15-test.txt");
+    let lines = include_str!("../input/day15-input.txt");
     //let mut sensors = Vec::new();
-    //let mut beacons = Vec::new();
+    let mut beacons = HashSet::new();
     let (_, l) = all_consuming(terminated(
         separated_list1(tag("\n"), parse_line),
         tag("\n"),
     ))(lines)
     .unwrap();
+    let mut coverage = HashSet::new();
+    let row = 2_000_000;
     for (sensor, beacon) in l {
-        let coverage = get_coverage(sensor, beacon);
-        print_coverage(coverage);
+        //println!("Getting coverage for {:?} b: {:?}", sensor, beacon);
+        coverage.extend(get_coverage(sensor, beacon, row));
+        beacons.insert(beacon);
     }
-
+    println!("{}", coverage.len());
+    //print_coverage(&coverage);
+    let positions: Vec<&Point> = coverage
+        .iter()
+        .filter(|&p| p.1 == row && !beacons.contains(p))
+        .collect();
+    println!("count: {}", positions.len());
     //println!("Sensors: {sensors:?}, Beacons: {beacons:?}");
 }
