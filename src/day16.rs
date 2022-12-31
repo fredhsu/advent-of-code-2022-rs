2f0e6c552e9ffcb5c4566d1c762b52f463f532d3
fredhsu <fredlhsu@gmail.com>
2022-12-30 18:51:57 -0800

    day16

--- src/day16.rs
+++ src/day16.rs
@@ -1,4 +1,5 @@
-use std::collections::{HashSet, VecDeque};
+use core::fmt;
+use std::collections::{HashMap, HashSet, VecDeque};
 
 use nom::bytes::complete::*;
 use nom::character::complete::*;
@@ -55,39 +56,46 @@ impl Valve {
 struct Valves {
     current: Valve,
     open_valves: HashSet<Valve>,
-    closed_valves: HashSet<Valve>,
+    closed_valves: HashMap<String, Valve>,
     flow: i64,
     time: i64,
 }
+impl fmt::Display for Valves {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        writeln!(f, "== Mintue {} ==", 30 - self.time);
+        write!(f, "Valves ");
+        for v in &self.open_valves {
+            write!(f, "{}, ", v.name);
+        }
+        write!(f, "are open, releasing {} pressure\n", self.flow)
+    }
+}
 impl Valves {
     fn open(&mut self) {
-        let valve = self.current.clone();
-        /*
-        let pos = self
-            .closed_valves
-            .iter()
-            .position(|v| v.name == valve)
-            .unwrap();
-        */
-        self.closed_valves.remove(&valve);
+        let mut valve = self.current.clone();
+        self.closed_valves.remove(&self.current.name);
         valve.open = true;
         self.time -= 1;
         self.flow += valve.flow * self.time;
-        println!("Opened valve: {valve:?}, flow is now {}", self.flow);
+        println!("{self}");
+        println!("You open valve {}.", &valve.name);
         self.open_valves.insert(valve);
     }
     fn distances(&self, target: &str) -> Vec<(i64, String)> {
+        println!("getting distance to {target} from {}", self.current.name);
         // bfs from current to all
         // only need to check closed valves
         let distances = Vec::new();
         let mut q = VecDeque::new();
         let visited = self.open_valves.clone();
-        q.push_back(self.open_valves.get(&self.current).unwrap());
-        while let Some(t) = q.pop_front() {
+        q.push_back(self.current.clone());
+        //while let Some(t) = q.pop_front() {
+        if let Some(t) = q.pop_front() {
             for c in &t.tunnels {
+                println!("Checking tunnel {c}");
                 // do a check here if there was the candidate found in closed
-                let v = self.closed_valves.iter().find(|&x| x.name == *c).unwrap();
-                q.push_back(v);
+                let v = self.closed_valves.iter().find(|&x| x.0 == c).unwrap();
+                q.push_back(v.1.clone());
             }
         }
         distances
@@ -95,29 +103,37 @@ impl Valves {
     fn move_to(&mut self, next: &str) {
         self.time -= 1;
         //self.current = next.to_string();
-        println!("Moving to {next}");
+        println!("{self}");
+        println!("You move to {next}");
     }
     fn get_candidates(&self) -> Vec<String> {
         // calculate flow * distance for each valve
-        self.closed_valves.iter().map(|v| v.name.clone()).collect()
+        self.closed_valves.iter().map(|v| v.0.clone()).collect()
     }
 }
 
 pub fn day_sixteen() {
     let lines = include_str!("../input/day16-test.txt").lines();
+    let mut closed_valves = HashMap::new();
+    for line in lines {
+        let valve = Valve::must_parse(line);
+        closed_valves.insert(valve.name.clone(), valve.clone());
+    }
+
+    let current = closed_valves.get("AA").unwrap().clone();
     let mut valves = Valves {
-        current: "AA".to_string(),
+        current,
         open_valves: HashSet::new(),
-        closed_valves: HashSet::new(),
+        closed_valves,
         flow: 0,
         time: 30,
     };
-    for line in lines {
-        valves.closed_valves.insert(Valve::must_parse(line));
-    }
     println!("{valves:?}");
     println!("candidates {:?}", valves.get_candidates());
     valves.move_to("BB");
+    let distance = valves.distances("BB");
+    println!("Distance to BB {:?}", distance);
+    println!("Distance to DD {:?}", valves.distances("DD"));
     println!("candidates {:?}", valves.get_candidates());
     valves.open();
     println!("candidates {:?}", valves.get_candidates());
